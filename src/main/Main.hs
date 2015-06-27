82376ef728575f2b5438965ae6626f02e6946b29
Koen De Keyser <koen.dekeyser@gmail.com>
2015-06-27 19:37:29 +0000

    Add IntelliJ project Define Commutating typeclass

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -1,5 +1,12 @@
 module Main where
 
+data CommutateResult = Commutates | DoesNotCommutate
+
+class Commutating a where
+  commutates :: a -> a -> CommutateResult
+
+
+
 main :: IO ()
 main =
   putStrLn "Halvin"
