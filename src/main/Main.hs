d8ae7773209adca334e4b59ff19979e8b7fa9a7b
Koen De Keyser <koen.dekeyser@gmail.com>
2015-06-29 06:03:55 +0000

    Some types

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeFamilies #-}
+
 module Main where
 
 data CommutateResult = Commutates | DoesNotCommutate
@@ -5,6 +7,20 @@ data CommutateResult = Commutates | DoesNotCommutate
 class Commutating a where
   commutates :: a -> a -> CommutateResult
 
+class Operation a where
+  data State a :: *
+  apply :: State a -> a b -> (State a, b)
+
+type Node = Integer
+
+type ClusterSize = Integer
+
+data Config = Config (ClusterSize,Node)
+
+data NodeState = NodeState {
+    nextPosAvailable :: Integer
+  ,
+  }
 
 
 main :: IO ()
